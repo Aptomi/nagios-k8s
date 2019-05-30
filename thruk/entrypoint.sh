f2d69066943657c158336af046481f52568a035e
Sergey Ryabin <ryabin.ss@gmail.com>
2019-05-30 05:58:43 +0000

    parametrized thrukadmin password

--- thruk/entrypoint.sh
+++ thruk/entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/sh -x
 rm /var/run/apache2/apache2.pid
 envsubst < /etc/thruk/thruk_local.conf | tee /etc/thruk/thruk_local.conf
+test -n "${THRUK_ADMIN_PASSWORD}" && htpasswd -b -c /etc/thruk/htpasswd thrukadmin ${THRUK_ADMIN_PASSWORD}
 apachectl -DFOREGROUND
